0c21ce49c950678a1535371f86817824f1d3880d
Erik Adelbert <erik@adelbert.fr>
2023-09-15 22:35:41 +0200

    Rework day15 for any input and improve efficiency

--- 2022/5/aoc5.go
+++ 2022/5/aoc5.go
@@ -5,17 +5,91 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	const (
-		Part1 = iota
-		Part2
-	)
+	var ws worlds // part1&2
 
-	// 	               [B] [L]     [J]
+	input := bufio.NewScanner(os.Stdin)
+	ws.load(input)
+
+	// read moves
+	for input.Scan() {
+		// input text: ^move (\d+) from (\d+) to (\d+)$
+		// args:          0    1     2    3    4   5
+		args := bytes.Fields(input.Bytes())
+		ws.move(args[5], args[3], args[1])
+	}
+
+	fmt.Println(ws)
+}
+
+// a world is a slice of adressable byte stacks
+type world [][]byte
+
+// for all byte stack methods:
+// s is source stack,
+// d is target stack,
+// n is number of stack elements
+
+func (w world) cut(s, n int) (x []byte) {
+	i := len(w[s])                   // stack top index
+	x, w[s] = w[s][i-n:], w[s][:i-n] // cut
+	return
+}
+
+func (w world) pop(s, n int) (x []byte) {
+	x = w.cut(s, n)
+	// reverse x
+	for l, r := 0, len(x)-1; l < r; {
+		x[l], x[r] = x[r], x[l]
+		l++
+		r--
+	}
+	return
+}
+
+func (w world) push(d int, x []byte) {
+	w[d] = append(w[d], x...)
+}
+
+func (w world) top(s int) byte {
+	i := len(w[s]) - 1 // byte stack top index
+	return w[s][i]
+}
+
+// worlds support muxed part 1&2 ops
+type worlds [2]world
+
+const (
+	Part1 = iota
+	Part2
+)
+
+func (ws worlds) String() string {
+	var sb strings.Builder
+
+	for i, s := range ws[Part1] {
+		if len(s) > 0 {
+			sb.WriteByte(ws[Part1].top(i))
+		}
+	}
+	sb.WriteByte('\n')
+	for i, s := range ws[Part2] {
+		if len(s) > 0 {
+			sb.WriteByte(ws[Part2].top(i))
+		}
+	}
+	return sb.String()
+}
+
+// read initial state from input
+// part 1&2 worlds start in the same state
+func (ws *worlds) load(input *bufio.Scanner) {
+	//                 [B] [L]     [J]
 	//             [B] [Q] [R]     [D] [T]
-	// 	           [G] [H] [H] [M] [N] [F]
+	//             [G] [H] [H] [M] [N] [F]
 	//         [J] [N] [D] [F] [J] [H] [B]
 	//     [Q] [F] [W] [S] [V] [N] [F] [N]
 	// [W] [N] [H] [M] [L] [B] [R] [T] [Q]
@@ -23,96 +97,52 @@ func main() {
 	// [S] [J] [S] [T] [T] [M] [D] [B] [H]
 	//  1   2   3   4   5   6   7   8   9
 
-	STACKS := [2][][]byte{
-		{ // part1
-			{},
-			[]byte("SLW"),
-			[]byte("JTNQ"),
-			[]byte("SCHFJ"),
-			[]byte("TRMWNGB"),
-			[]byte("TRLSDHQB"),
-			[]byte("MJBVFHRL"),
-			[]byte("DWRNJM"),
-			[]byte("BZTFHNDJ"),
-			[]byte("HLQNBFT"),
-		},
-		{ // part2
-			{},
-			[]byte("SLW"),
-			[]byte("JTNQ"),
-			[]byte("SCHFJ"),
-			[]byte("TRMWNGB"),
-			[]byte("TRLSDHQB"),
-			[]byte("MJBVFHRL"),
-			[]byte("DWRNJM"),
-			[]byte("BZTFHNDJ"),
-			[]byte("HLQNBFT"),
-		},
-	}
-
-	rev := func(x []byte) []byte {
-		for i, j := 0, len(x)-1; i < j; i, j = i+1, j-1 {
-			x[i], x[j] = x[j], x[i]
-		}
-		return x
-	}
-
-	// for top, pop, cut and push:
-	//   p: Part1|Part2,
-	//   s: source stack,
-	//   d: target stack
-
-	top := func(p, s int) byte {
-		i := len(STACKS[p][s]) - 1 // stack top index
-		return STACKS[p][s][i]
-	}
-
-	cut := func(p, s, n int) []byte {
-		i := len(STACKS[p][s]) // stack top index
-		x := STACKS[p][s][i-n:]
-		STACKS[p][s] = STACKS[p][s][:i-n] // cut
-		return x
-	}
-
-	pop := func(p, s, n int) []byte {
-		return rev(cut(p, s, n))
-	}
-
-	push := func(p, d int, x []byte) {
-		STACKS[p][d] = append(STACKS[p][d], x...)
-	}
-
-	move := func(nel, src, dst []byte) {
-		n, s, d := atoi(nel), atoi(src), atoi(dst)
-
-		push(Part1, d, pop(Part1, s, n))
-		push(Part2, d, cut(Part2, s, n))
-	}
-
-	input := bufio.NewScanner(os.Stdin)
+	lines := make([][]byte, 0, 8)
 	for input.Scan() {
-		// input text: ^move (\d+) from (\d+) to (\d+)$
-		// args:          0    1     2    3    4   5
-		args := bytes.Fields(input.Bytes())
-		move(args[1], args[3], args[5])
+		raw := input.Bytes()
+		if len(raw) == 0 {
+			// done reading world state
+			break
+		}
+
+		line := make([]byte, 0, 16)
+		// copy letters only
+		// see ex. input above
+		for i := 1; i < len(raw); i += 4 {
+			line = append(line, raw[i])
+		}
+		lines = append(lines, line)
 	}
 
-	display := func(p int) {
-		var out bytes.Buffer
-		for i, s := range STACKS[p] {
-			if len(s) > 0 {
-				out.WriteByte(top(p, i))
+	// demux
+	for len(ws[Part1]) <= len(lines[0]) {
+		ws[Part1] = append(ws[Part1], make([]byte, 0, 16))
+		ws[Part2] = append(ws[Part2], make([]byte, 0, 16))
+	}
+
+	// rotate lines to bstacks
+	for i := len(lines) - 2; i >= 0; i-- { // discard last line
+		for j, c := range lines[i] {
+			if c != ' ' {
+				// demux
+				ws[Part1][j+1] = append(ws[Part1][j+1], c) // 1-indexed
+				ws[Part2][j+1] = append(ws[Part2][j+1], c) // 1-indexed
 			}
 		}
-		fmt.Println(out.String())
 	}
+}
 
-	display(Part1)
-	display(Part2)
+// muxed move for part1&2 worlds
+func (ws worlds) move(dst, src, size []byte) {
+	d, s, n := atoi(dst), atoi(src), atoi(size)
+
+	// demux
+	ws[Part1].push(d, ws[Part1].pop(s, n))
+	ws[Part2].push(d, ws[Part2].cut(s, n))
 }
 
 // strconv.Atoi simplified core loop
-// s is ^\d+$
+// s is ^(\d+)$
 func atoi(s []byte) int {
 	var n int
 	for _, c := range s {
