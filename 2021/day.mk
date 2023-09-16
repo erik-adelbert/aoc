22e07af963fcff29aec9eea599bcc37159066318
Erik Adelbert <erik@adelbert.fr>
2023-09-17 00:52:47 +0200

    Fix 2021 Makefile

---
+++ 2021/day.mk
@@ -0,0 +1,44 @@
+GOC = go build
+GOV = go vet
+
+BENCH = ../bench.sh
+
+DOWNLOAD = ../download/main.go
+
+EX = sample.txt
+IN = input.txt
+
+BIN = $(addprefix aoc,$(shell basename $(CURDIR)))
+SRC = $(BIN).go
+
+bench: build
+	$(BENCH) $(BIN)
+	@$(MAKE) clean
+
+build: input.txt
+	$(GOC) $(SRC)
+
+check:
+	$(GOV) $(SRC)
+
+clean:
+	go clean
+	rm -f $(BIN)
+
+input.txt:
+	go run $(DOWNLOAD)
+
+cpuprof: build
+	./$(BIN) -cpuprofile=$(BIN).cpu.prof < $(IN)
+
+memprof: build
+	./$(BIN) -memprofile=$(BIN).mem.prof < $(IN)
+
+run: input.txt
+	go run ./$(SRC) < $(IN)
+
+sample:
+	go run ./$(SRC) < $(EX)
+
+
+.PHONY: bench build check clean cpuprof exemple memprof run
