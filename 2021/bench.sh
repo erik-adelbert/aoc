bf5c524a5d47053580c87fae7ea2024159112243
Erik Adelbert <erik@adelbert.fr>
2023-09-17 00:45:24 +0200

    Update 2021 bench.sh

--- 2021/bench.sh
+++ 2021/bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hyperfine --warmup 5 --min-runs 1000 "cat input.txt" "cat input.txt | ./${1}" --export-markdown time.md
+hyperfine --warmup 100 "cat input.txt > /dev/null" "./${1} < input.txt" --export-markdown time.md
