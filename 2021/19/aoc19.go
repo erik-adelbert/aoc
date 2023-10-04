e4cc411768b92feb0cc759f98d76eef725e27366
Erik Adelbert <erik@adelbert.fr>
2023-10-04 14:04:24 +0200

    Rework 2021/day19

--- 2021/19/aoc19.go
+++ 2021/19/aoc19.go
@@ -1,13 +1,98 @@
+// aoc19.go --
+// advent of code 2021 day 19
+//
+// https://adventofcode.com/2021/day/19
+// https://github.com/erik-adelbert/aoc
+//
+// (ɔ) Erik Adelbert - erik_AT_adelbert_DOT_fr
+// -------------------------------------------
+// 2021-12-19: initial commit
+
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
+	"slices"
 	"sort"
 	"strconv"
 	"strings"
 )
+
+var (
+	reads []reading
+	scans reading
+	fixed map[vec]bool
+)
+
+// niladic init
+//
+// https://go.dev/doc/effective_go#initialization
+func init() {
+	reads = make([]reading, 0, 32)
+	scans = append(make([]vec, 0, 27), vec{0, 0, 0}) // origin
+	fixed = make(map[vec]bool, 337)
+}
+
+func main() {
+	points := make(reading, 0, 32)
+
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		switch line := input.Text(); line != "" {
+		default:
+			args := strings.Split(line, ",") // trust input
+			x := atoi(args[0])
+			y := atoi(args[1])
+			z := atoi(args[2])
+			points = append(points, vec{x, y, z})
+		case strings.HasPrefix(line, "---"):
+			if len(points) > 0 {
+				reads = append(reads, Reading(points)) // clone points
+				points = points[:0]                    // reset
+			}
+		}
+	}
+	if points != nil {
+		reads = append(reads, points) // last reading
+	}
+
+	// dice trick!
+	// runtime x ms ± 22% relative to readings order
+	rand.Shuffle(len(reads), func(i, j int) {
+		reads[i], reads[j] = reads[j], reads[i]
+	})
+
+	for _, p := range reads[0] { // origin
+		fixed[p] = true
+	}
+	reads = reads[1:] // shift
+
+	// align all readings gradually
+	for len(reads) > 0 {
+		i := 0
+		for _, r := range reads {
+			if !ralign(r) { // populate scans[] and fixed[]
+				reads[i] = r // no match yet, push back
+				i++
+			}
+		}
+		reads = reads[:i] // pushed back
+	}
+	fmt.Println(len(fixed)) // part1
+
+	diam := 0
+	for i, v0 := range scans {
+		for _, v1 := range scans[i+1:] {
+			if dist := v1.sub(v0).manh(); dist > diam {
+				diam = dist // max
+			}
+		}
+	}
+	fmt.Println(diam) // part2
+}
 
 // Axis
 const (
@@ -17,12 +102,6 @@ const (
 )
 
 type vec [3]int
-
-func (v vec) sign(s vec) vec {
-	return vec{
-		s[X] * v[X], s[Y] * v[Y], s[Z] * v[Z],
-	}
-}
 
 func (v vec) sub(u vec) vec {
 	return vec{
@@ -38,35 +117,42 @@ type reading []vec
 
 // Reading is a vector field abstraction
 func Reading(points []vec) reading {
-	return append(points[:0:0], points...)
+	buf := make(reading, len(points))
+	copy(buf, points) // clone
+	return buf
 }
 
 type rotator func() reading // rotations iterator
 
 func (r reading) π2rots() rotator {
 	rots := []struct {
-		s, a vec // sign, axis
+		s, a vec // sign, axis order
 	}{
 		{vec{-1, -1, +1}, vec{X, Y, Z}},
 		{vec{-1, +1, -1}, vec{X, Y, Z}},
 		{vec{+1, -1, -1}, vec{X, Y, Z}},
 		{vec{+1, +1, +1}, vec{X, Y, Z}},
+
 		{vec{-1, -1, -1}, vec{X, Z, Y}},
 		{vec{-1, +1, +1}, vec{X, Z, Y}},
 		{vec{+1, -1, +1}, vec{X, Z, Y}},
 		{vec{+1, +1, -1}, vec{X, Z, Y}},
+
 		{vec{-1, -1, -1}, vec{Y, X, Z}},
 		{vec{-1, +1, +1}, vec{Y, X, Z}},
 		{vec{+1, -1, +1}, vec{Y, X, Z}},
 		{vec{+1, +1, -1}, vec{Y, X, Z}},
+
 		{vec{-1, -1, +1}, vec{Y, Z, X}},
 		{vec{-1, +1, -1}, vec{Y, Z, X}},
 		{vec{+1, -1, -1}, vec{Y, Z, X}},
 		{vec{+1, +1, +1}, vec{Y, Z, X}},
+
 		{vec{-1, -1, +1}, vec{Z, X, Y}},
 		{vec{-1, +1, -1}, vec{Z, X, Y}},
 		{vec{+1, -1, -1}, vec{Z, X, Y}},
 		{vec{+1, +1, +1}, vec{Z, X, Y}},
+
 		{vec{-1, -1, -1}, vec{Z, Y, X}},
 		{vec{-1, +1, +1}, vec{Z, Y, X}},
 		{vec{+1, -1, +1}, vec{Z, Y, X}},
@@ -76,47 +162,40 @@ func (r reading) π2rots() rotator {
 	i, turned := 0, make(reading, len(r))
 
 	return func() reading { // rotator
-		if i >= len(rots) {
-			return reading(nil)
+		if i >= len(rots) { // capture i
+			return nil
 		}
 
 		rot := rots[i]
 		for j, v := range r {
-			turned[j] = vec{
-				v[rot.a[X]], v[rot.a[Y]], v[rot.a[Z]],
-			}.sign(rot.s)
+			// π/2 rotation
+			// shuffle X, Y, Z according to sign and axis order
+			s, a := &rot.s, &rot.a
+			turned[j] = vec{ // capture turned
+				s[0] * v[a[0]], s[1] * v[a[1]], s[2] * v[a[2]],
+			}
 		}
 		i++
-		return turned
+		return turned // leak turned
 	}
 }
 
-var (
-	reads []reading
-	scans reading
-	fixed map[vec]bool
-)
-
-func init() {
-	reads = make([]reading, 0, 32)
-	scans = append(make([]vec, 0, 28), vec{0, 0, 0})
-	fixed = make(map[vec]bool, 337)
-}
-
 func list(m map[vec]bool) reading {
-	list := make(reading, 0, len(m))
+	list := make(reading, len(m))
+
+	i := 0
 	for v := range m {
-		list = append(list, v)
+		list[i] = v
+		i++
 	}
 	return list
 }
 
 func difs(r reading) reading {
 	difs := make([]vec, len(r)-1)
-	for i, v1 := range r[1:] { // v0, v1 = r[0], r[1] ...
-		difs[i] = v1.sub(r[i]) // difs[0] == v1-v0, ...
+	for i, v1 := range r[1:] { // i, v1 = {0, r[1]}, {1, r[2]}, ...
+		difs[i] = v1.sub(r[i]) // difs[0] = [r[1]-r[0], r[2]-r[1] ...]
 	}
-
 	return difs
 }
 
@@ -129,10 +208,12 @@ func inter(a, b reading, first bool) reading {
 	for _, v := range a {
 		m[v] = true
 	}
+
 	inter := make(reading, 0, len(m))
 	for _, v := range b {
 		if m[v] {
-			if inter = append(inter, v); first {
+			inter = append(inter, v)
+			if first {
 				break
 			}
 		}
@@ -148,59 +229,7 @@ func rebase(r reading, o vec) reading {
 	return based
 }
 
-func main() {
-	points := make(reading, 0, 32)
-
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		switch line := input.Text(); line != "" {
-		case strings.Contains(line, "---"):
-			if len(points) > 0 {
-				reads = append(reads, Reading(points)) // clone points
-				points = points[:0]                    // reset
-			}
-		default:
-			args := strings.Split(line, ",")
-			x, _ := strconv.Atoi(args[0])
-			y, _ := strconv.Atoi(args[1])
-			z, _ := strconv.Atoi(args[2])
-			points = append(points, vec{x, y, z})
-		}
-	}
-	if points != nil {
-		reads = append(reads, points) // last reading
-	}
-
-	for _, p := range reads[0] {
-		fixed[p] = true
-	}
-
-	cur := reads[1:]
-	for len(cur) > 0 {
-		i := 0
-		for _, r := range cur {
-			if !rotal(r) {
-				cur[i] = r
-				i++
-			}
-		}
-		cur = cur[:i]
-	}
-
-	fmt.Println(len(fixed)) // part1
-
-	diam := 0
-	for i, v0 := range scans {
-		for _, v1 := range scans[i+1:] {
-			if dist := v1.sub(v0).manh(); dist > diam {
-				diam = dist
-			}
-		}
-	}
-	fmt.Println(diam) // part2
-}
-
-func rotal(r reading) bool {
+func ralign(r reading) bool {
 	next := r.π2rots()
 
 	rot := next()
@@ -214,14 +243,21 @@ func rotal(r reading) bool {
 }
 
 func align(r reading) bool {
-	index := func(r reading, x vec) int {
-		for i, v := range r {
-			if x == v {
-				return i
-			}
-		}
-		return len(r)
+
+	// index := func(r reading, x vec) int {  // older Go
+	// 	for i, v := range r {
+	// 		if x == v {
+	// 			return i
+	// 		}
+	// 	}
+	// 	return len(r)
+	// }
+
+	index := func(r reading, x vec) int { // Go 1.21
+		return slices.Index(r, x)
 	}
+
+	const TRESH = 12 // from challenge
 
 	known := list(fixed)
 	for a := X; a <= Z; a++ { // X, Y, Z
@@ -236,18 +272,18 @@ func align(r reading) bool {
 		rdifs, kdifs := difs(r), difs(known)
 
 		const (
-			All   = false
-			First = !All
+			ALL   = false
+			FIRST = !ALL
 		)
 
-		if matches := inter(rdifs, kdifs, First); len(matches) == 1 {
+		if matches := inter(rdifs, kdifs, FIRST); len(matches) == 1 {
 			pivot := matches[0]
 			i := index(rdifs, pivot)
 			j := index(kdifs, pivot)
 			o := r[i].sub(known[j])
-
 			rebased := rebase(r, o)
-			if len(inter(known, rebased, All)) >= 12 {
+
+			if len(inter(known, rebased, ALL)) >= TRESH {
 				for _, v := range rebased {
 					fixed[v] = true
 				}
@@ -264,4 +300,9 @@ func abs(a int) int {
 		return a
 	}
 	return -a
+}
+
+func atoi(s string) int {
+	n, _ := strconv.Atoi(s) // trust s
+	return n
 }
