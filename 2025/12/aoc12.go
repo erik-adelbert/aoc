c511fc41106437b0ba3a87396f2796667b18d359
Erik Adelbert <erik@adelbert.fr>
2025-12-12 16:01:11 +0100

    Normalize 25/12

--- 2025/12/aoc12.go
+++ 2025/12/aoc12.go
@@ -28,12 +28,12 @@ func main() {
 
 	var acc1 int // part 1 accumulator
 
-	input := bufio.NewScanner(os.Stdin)
-
 	polys := make([]polyomino, 0, PolyominoCount)
 	block := make([][]byte, 0, PolyominoDim)
 
 	// read all input lines
+	input := bufio.NewScanner(os.Stdin)
+
 	for i := 0; input.Scan(); i++ {
 		buf := input.Bytes()
 		maxPoly := PolyominoCount * (PolyominoDim + 2)
@@ -48,32 +48,26 @@ func main() {
 
 		case i < maxPoly && i%(PolyominoDim+2) == 0:
 			// skip header line with index
+
 		case i < maxPoly:
 			// polyomino line
 			block = append(block, buf)
 
 		default:
 			// processing grid line: "WxH: n1 n2 n3 ..."
-			lhs, rhs, _ := bytes.Cut(buf, []byte(": "))  // ["WxH", " n1 n2 n3 ..."]
-			wbuf, hbuf, _ := bytes.Cut(lhs, []byte("x")) // ["W", "H"]
-			fields := bytes.Fields(rhs)
+			lhs, rhs, _ := bytes.Cut(buf, []byte(": ")) // ["WxH", " n1 n2 n3 ..."]
+			w, h, _ := bytes.Cut(lhs, []byte("x"))      // ["W", "H"]
 
-			w, h := atoi(wbuf), atoi(hbuf)
-			area := w * h
+			area := atoi(w) * atoi(h)
 
 			// compute total required cells
 			size := 0
-			for j, f := range fields {
-				if j >= len(polys) {
-					break
-				}
-				num := atoi(f)
-				size += num * polys[j].area
+			for j, n := range bytes.Fields(rhs) {
+				size += atoi(n) * polys[j].area
 			}
 
 			// apply empirical 87% heuristic
-			if size*115 < area*100 {
-				// if total area used is less than 87% of grid area, count it
+			if size*23 < area*20 {
 				acc1++
 			}
 		}
